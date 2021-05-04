29dfd6f97425a4a580cba443b6f6f1cd47637d26
gnantheerth <gnantheerthofficial@gmail.com>
2021-05-04 20:42:34 +0530

    KeywordsAndExpressions

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        boolean gameOver=true;
+        int score=800;
+        int levelCompleted=5;
+        int bonus=100;
+
+    int highscore= calculatescore(true,800,5,100);
+        System.out.println("your final score was" + highscore);
+    score=10000;
+    levelCompleted=8;
+    bonus=200;
+
+    highscore= calculatescore(true,10000,8,200);
+        System.out.println("your final score was" + highscore);
+
+    }
+    public static int calculatescore(boolean gameOver, int score, int levelCompleted,int bonus){
+
+        if (gameOver==true) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore +=2000;
+
+            return finalScore;
+        }
+        return -1;
+    }
+
+
+    }
+
+    
+
